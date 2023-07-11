80095458476581f9d26094807a5562996c57a49a
mikekarim07 <135273602+mikekarim07@users.noreply.github.com>
2023-07-11 11:34:18 -0600

    Update xmlreader.py

--- xmlreader.py
+++ xmlreader.py
@@ -1,97 +1,40 @@
 import streamlit as st
-import zipfile
+import os
 import xml.etree.ElementTree as ET
 
-def parse_xml4(xml_file):
-    tree = ET.parse(xml_file)
-    root = tree.getroot()
-
-    namespaces = {
-        'cfdi': 'http://www.sat.gob.mx/cfd/4',
-        'tfd': 'http://www.sat.gob.mx/TimbreFiscalDigital',
-        'pago10': 'http://www.sat.gob.mx/Pagos'
+def extract_xml_data(xml_content):
+    # Lógica para extraer información del XML y devolver un diccionario
+    # Personaliza esta función según tus necesidades
+    root = ET.fromstring(xml_content)
+    data = {
+        'version': root.attrib.get('Version', ''),
+        'forma_de_pago': root.attrib.get('FormaPago', ''),
+        'regimen': root.find('cfdi:Emisor', namespaces=namespaces).attrib.get('RegimenFiscal', ''),
+        'tipo_de_comprobante': root.attrib.get('TipoDeComprobante', ''),
+        'rfc_emisor': root.find('cfdi:Emisor', namespaces=namespaces).attrib.get('Rfc', ''),
+        'nombre_emisor': root.find('cfdi:Emisor', namespaces=namespaces).attrib.get('Nombre', ''),
+        'rfc_receptor': root.find('cfdi:Receptor', namespaces=namespaces).attrib.get('Rfc', ''),
+        'nombre_receptor': root.find('cfdi:Receptor', namespaces=namespaces).attrib.get('Nombre', ''),
+        'subtotal': root.attrib.get('SubTotal', ''),
+        'total': root.attrib.get('Total', ''),
+        'uuid': root.find('cfdi:Complemento/tfd:TimbreFiscalDigital', namespaces=namespaces).attrib.get('UUID', ''),
+        'fecha_emision': root.attrib.get('Fecha', '')
     }
-
-    version = root.attrib.get('Version', '')
-    forma_de_pago = root.attrib.get('FormaPago', '')
-    regimen = root.find('cfdi:Emisor', namespaces=namespaces).attrib.get('RegimenFiscal', '')
-    tipo_de_comprobante = root.attrib.get('TipoDeComprobante', '')
-    rfc_emisor = root.find('cfdi:Emisor', namespaces=namespaces).attrib.get('Rfc', '')
-    nombre_emisor = root.find('cfdi:Emisor', namespaces=namespaces).attrib.get('Nombre', '')
-    rfc_receptor = root.find('cfdi:Receptor', namespaces=namespaces).attrib.get('Rfc', '')
-    nombre_receptor = root.find('cfdi:Receptor', namespaces=namespaces).attrib.get('Nombre', '')
-    subtotal = root.attrib.get('SubTotal', '')
-    total = root.attrib.get('Total', '')
-    uuid = root.find('cfdi:Complemento/tfd:TimbreFiscalDigital', namespaces=namespaces).attrib.get('UUID', '')
-    fecha_emision = root.attrib.get('Fecha', '')
-
-    return {
-        'version': version,
-        'forma_de_pago': forma_de_pago,
-        'regimen': regimen,
-        'tipo_de_comprobante': tipo_de_comprobante,
-        'rfc_emisor': rfc_emisor,
-        'nombre_emisor': nombre_emisor,
-        'rfc_receptor': rfc_receptor,
-        'nombre_receptor': nombre_receptor,
-        'subtotal': subtotal,
-        'total': total,
-        'uuid': uuid,
-        'fecha_emision': fecha_emision
-    }
-
-def parse_xml33(xml_file):
-    tree = ET.parse(xml_file)
-    root = tree.getroot()
-
-    namespaces = {
-        'cfdi': 'http://www.sat.gob.mx/cfd/3',
-        'tfd': 'http://www.sat.gob.mx/TimbreFiscalDigital',
-        'pago10': 'http://www.sat.gob.mx/Pagos'
-    }
-
-    version = root.attrib.get('Version', '')
-    forma_de_pago = root.attrib.get('FormaPago', '')
-    regimen = root.find('cfdi:Emisor', namespaces=namespaces).attrib.get('RegimenFiscal', '')
-    tipo_de_comprobante = root.attrib.get('TipoDeComprobante', '')
-    rfc_emisor = root.find('cfdi:Emisor', namespaces=namespaces).attrib.get('Rfc', '')
-    nombre_emisor = root.find('cfdi:Emisor', namespaces=namespaces).attrib.get('Nombre', '')
-    rfc_receptor = root.find('cfdi:Receptor', namespaces=namespaces).attrib.get('Rfc', '')
-    nombre_receptor = root.find('cfdi:Receptor', namespaces=namespaces).attrib.get('Nombre', '')
-    subtotal = root.attrib.get('SubTotal', '')
-    total = root.attrib.get('Total', '')
-    uuid = root.find('cfdi:Complemento/tfd:TimbreFiscalDigital', namespaces=namespaces).attrib.get('UUID', '')
-    fecha_emision = root.attrib.get('Fecha', '')
-    
-    return {
-        'version': version,
-        'forma_de_pago': forma_de_pago,
-        'regimen': regimen,
-        'tipo_de_comprobante': tipo_de_comprobante,
-        'rfc_emisor': rfc_emisor,
-        'nombre_emisor': nombre_emisor,
-        'rfc_receptor': rfc_receptor,
-        'nombre_receptor': nombre_receptor,
-        'subtotal': subtotal,
-        'total': total,
-        'uuid': uuid,
-        'fecha_emision': fecha_emision
-    }
+    return data
 
 def main():
-    uploaded_files = st.file_uploader('Cargar archivos ZIP', accept_multiple_files=True, type='zip')
+    uploaded_folder = st.file_uploader('Cargar carpeta', type='folder')
 
-    for uploaded_file in uploaded_files:
-        if uploaded_file:
-            with zipfile.ZipFile(uploaded_file, 'r') as zip_ref:
-                xml_files = [file for file in zip_ref.namelist() if file.endswith('.xml')]
+    if uploaded_folder:
+        folder_path = st.text_input('Ruta de la carpeta:', value=uploaded_folder.name)
+        xml_files = [file for file in os.listdir(folder_path) if file.endswith('.xml')]
 
-                for xml_file in xml_files:
-                    xml_content = zip_ref.read(xml_file)
-                    data = extract_xml_data(xml_content)
+        for xml_file in xml_files:
+            xml_content = open(os.path.join(folder_path, xml_file), 'rb').read()
+            data = extract_xml_data(xml_content)
 
-                    # Haz lo que desees con los datos extraídos del XML
-                    st.write(data)
+            # Haz lo que desees con los datos extraídos del XML
+            st.write(data)
 
 if __name__ == '__main__':
     main()
